c9bcc3f4e54326ecc066c2cb22ecaa950513b4e2
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-11 12:03:58 +0530

    - removed css assets and replaced them with sass files - added css-materialize module - integrated materialize-css via index.sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import $ from 'jquery'
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   componentDidMount() {
@@ -13,20 +11,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>
+          PS Leaderboard...
+        </h1>
       </div>
     );
   }
