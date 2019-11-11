a71c1e23faaa7e2db5a37177ce6e7d1986e46ef5
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-11 16:52:14 +0530

    - Setting up new-design WIP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
-import {sum, values} from 'lodash'
 import $ from 'jquery'
 import './App.sass'
+import ImageShield from './components/ImageShield'
 
 class App extends Component {
   componentDidMount() {
@@ -11,57 +11,24 @@ class App extends Component {
     }, 2000)
   }
   render() {
-    const hawkings = {
-      teamName: 'Hawkings',
-      score: {
-        1: 20,
-        2: 20,
-        3: 40,
-        4: 12,
-        5: 11,
-        6: 16,
-        7: 10,
-      },
-    }
+    // const hawkings = {
+    //   teamName: 'Hawkings',
+    //   score: {
+    //     1: 20,
+    //     2: 20,
+    //     3: 40,
+    //     4: 12,
+    //     5: 11,
+    //     6: 16,
+    //     7: 10,
+    //   },
+    // }
     return (
       <div className="App">
         <div className="container">
-          <div className="card">
-            <div className="card-content">
-              <h4>Leader Board</h4>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Team Name</th>
-                    <th>Game 1</th>
-                    <th>Game 2</th>
-                    <th>Game 3</th>
-                    <th>Game 4</th>
-                    <th>Game 5</th>
-                    <th>Game 6</th>
-                    <th>Game 7</th>
-                    <th>Total</th>
-                  </tr>
-                </thead>
-
-                <tbody>
-                  <tr>
-                    <td>{hawkings.teamName}</td>
-                    {
-                      Object.keys(hawkings.score)
-                          .map((key)=> (
-                            <td key={`hawking-score-${key}`}>
-                              {hawkings.score[key]}
-                            </td>
-                          ))
-                    }
-                    <td>
-                      { sum(values(hawkings.score))}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+          <div className="row">
+            <h4>Leader Board</h4>
+            <ImageShield />
           </div>
         </div>
       </div>
---
+++ src/components/ImageShield/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class ImageShield extends Component {
+  render() {
+    return (
+      <div className="white">
+
+      </div>
+    )
+  }
+}
+
+export default ImageShield
