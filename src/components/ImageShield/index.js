3596a081fce9affd4db9d235a8664629ee8c9d51
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-11 17:00:44 +0530

    - Added styles for ImageShield - Wrapped it in card class for default card-styles

--- src/components/ImageShield/index.js
+++ src/components/ImageShield/index.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
+import './styles.sass'
 
 class ImageShield extends Component {
   render() {
     return (
-      <div className="white">
-
+      <div className="ImageShield white card">
+        <img src="/img/smiley.png" alt="smiley plus shield" />
       </div>
     )
   }
