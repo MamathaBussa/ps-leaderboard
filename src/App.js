3c8dba2c35cf7c581ddff657d52d0a79ce53fe97
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-13 15:38:02 +0530

    working score-card styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
 import $ from 'jquery'
 import './App.sass'
-import Carousel from './components/Carousel'
+// import Carousel from './components/Carousel'
+import CurrentComponent from './components/SmileyShieldAndTeamName'
 
 class App extends Component {
   componentDidMount() {
@@ -14,7 +15,12 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container">
-          <Carousel />
+          <CurrentComponent componentData={{
+            teamName: 'Awesomeness',
+          }}/>
+          <h4 className="ScoreCard card green darken-1 white-text">
+            40
+          </h4>
         </div>
       </div>
     )
