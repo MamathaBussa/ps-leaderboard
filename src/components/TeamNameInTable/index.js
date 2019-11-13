92a505b892ec490c3e053dc9c35ed3e753bb8e06
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-13 08:43:16 +0530

    added rankings

--- src/components/TeamNameInTable/index.js
+++ src/components/TeamNameInTable/index.js
@@ -6,7 +6,11 @@ class TeamNameInTable extends Component {
     return (
       <div className="TeamNameInTable">
         <div className="card">
-          <h4 className="deep-orange-text">Hawkings...</h4>
+          <h4 className="deep-orange-text">
+            <span className="purple-text">#1</span>
+              &nbsp;
+              Hawkings
+          </h4>
           <div className="score light-green white-text card">
             +30
           </div>
