425ae83626530cf1ea690c4bd06f9615bdbc0ee0
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-14 09:39:58 +0530

    :whale: - TeamDetailedScoreRow component - initial setup

---
+++ src/components/TeamDetailedScoreRow/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './styles.sass'
+
+class TeamDetailsScoreRow extends Component {
+  render() {
+    return (
+      <div className="TeamDetailsScoreRow">
+        TeamDetailsScoreRow
+      </div>
+    )
+  }
+}
+
+export default TeamDetailsScoreRow
