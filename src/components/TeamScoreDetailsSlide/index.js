9a3a6e422eca37b3fff5055514e15536c879deca
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-14 09:56:05 +0530

    - :whale: extracted data

--- src/components/TeamScoreDetailsSlide/index.js
+++ src/components/TeamScoreDetailsSlide/index.js
@@ -1,38 +1,11 @@
 import React, {Component} from 'react'
 import TeamDetailedScoreRow from '../TeamDetailedScoreRow'
-// import ImageShield from '../ImageShield'
-// import TeamNameInTable from '../TeamNameInTable'
+import {TeamScoreDetails} from '../../data'
 import './styles.sass'
 
 class TeamScoreDetailsSlide extends Component {
   render() {
-    const componentDataList = [{
-      teamName: 'Falcons',
-      smileyType: 1,
-      totalScore: 129,
-      scores: [
-        12,
-        15,
-        11,
-        null,
-        null,
-        null,
-        null,
-      ],
-    }, {
-      teamName: 'Giants',
-      smileyType: 2,
-      totalScore: 118,
-      scores: [
-        11,
-        12,
-        19,
-        null,
-        null,
-        null,
-        null,
-      ],
-    }]
+    const componentDataList = TeamScoreDetails
     return (
       <div className="TeamScoreDetailsSlide card blue lighten-2">
         <table>
---
+++ src/data/index.js
@@ -0,0 +1,27 @@
+export const TeamScoreDetails = [{
+  teamName: 'Falcons',
+  smileyType: 1,
+  totalScore: 129,
+  scores: [
+    12,
+    15,
+    11,
+    null,
+    null,
+    null,
+    null,
+  ],
+}, {
+  teamName: 'Giants...',
+  smileyType: 2,
+  totalScore: 118,
+  scores: [
+    11,
+    12,
+    19,
+    null,
+    null,
+    null,
+    null,
+  ],
+}]
