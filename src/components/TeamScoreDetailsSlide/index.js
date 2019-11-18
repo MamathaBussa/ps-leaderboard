f37a4b6f76203c8e5476e978689f6f3dd38367f0
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-18 12:21:07 +0530

    - Added cards for team-name and game-name

--- src/components/TeamScoreDetailsSlide/index.js
+++ src/components/TeamScoreDetailsSlide/index.js
@@ -8,6 +8,41 @@ class TeamScoreDetailsSlide extends Component {
     return (
       <div className="TeamScoreDetailsSlide card blue lighten-2">
         <table>
+          <thead>
+            <tr>
+              <th className="brown-text text-darken-3 center">
+                <h5 className="card">
+                  Team Name
+                </h5>
+              </th>
+              <th className="brown-text text-darken-3 center">
+                <h5 className="card">
+                  Game 1 The Quiz!
+                </h5>
+              </th>
+              <th className="brown-text text-darken-3 center">
+                <h5 className="card">Game 2</h5>
+              </th>
+              <th className="brown-text text-darken-3 center">
+                <h5 className="card">Game 3</h5>
+              </th>
+              <th className="brown-text text-darken-3 center">
+                <h5 className="card">Game 4</h5>
+              </th>
+              <th className="brown-text text-darken-3 center">
+                <h5 className="card">Game 5</h5>
+              </th>
+              <th className="brown-text text-darken-3 center">
+                <h5 className="card">Game 6</h5>
+              </th>
+              <th className="brown-text text-darken-3 center">
+                <h5 className="card">Game 7</h5>
+              </th>
+              <th className="brown-text text-darken-3 center">
+                <h5 className="card">Total</h5>
+              </th>
+            </tr>
+          </thead>
           <tbody>
             {
               componentData.map((eachComponentData, i) =>(
