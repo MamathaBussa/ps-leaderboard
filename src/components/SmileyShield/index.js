84e8a996f2d075aa48a46db3134aba355d947b64
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-20 07:37:16 +0530

    adding styles with outlines to see position of each element

--- src/components/SmileyShield/index.js
+++ src/components/SmileyShield/index.js
@@ -5,7 +5,7 @@ class SmileyShield extends Component {
   render() {
     const {teamShieldUrl, teamName}=this.props.componentData
     return (
-      <div className="SmileyShield">
+      <div className="SmileyShield card">
         <img src={teamShieldUrl} alt={teamName} title={teamName} />
       </div>
     )
