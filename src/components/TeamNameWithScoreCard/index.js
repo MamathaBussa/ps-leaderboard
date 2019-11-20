c5272734dc095aa5bffe1ff3595fb548cce861b7
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-20 12:23:07 +0530

    adjusted padding and margin for few components to fit content within the page to accomodate logo

--- src/components/TeamNameWithScoreCard/index.js
+++ src/components/TeamNameWithScoreCard/index.js
@@ -7,7 +7,7 @@ class TeamNameWithScoreCard extends Component {
   render() {
     const {componentData} = this.props
     return (
-      <div className="TeamNameWithScoreCard">
+      <div className="TeamNameWithScoreCard card transparent">
         <TeamNameImage componentData={componentData} />
         <ScoreCard componentData={componentData} />
       </div>
