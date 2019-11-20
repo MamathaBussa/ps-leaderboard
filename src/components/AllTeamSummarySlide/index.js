1068723acd4fe386acd5cd64055983c25f8dc43e
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-20 11:53:28 +0530

    - Added parchment background - removed all-team summary background styles

--- src/components/AllTeamSummarySlide/index.js
+++ src/components/AllTeamSummarySlide/index.js
@@ -12,7 +12,7 @@ class AllTeamsSummarySlide extends Component {
         .filter((data, i) => i % 2 !== 0)
 
     return (
-      <div className="AllTeamsSummarySlide card yellow lighten-4">
+      <div className="AllTeamsSummarySlide">
         <div className="TableWrapper">
           <table>
             <tbody>
