9838a5c4840f625d68e7f42281a9698e8c866f66
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-21 14:22:53 +0530

    Updated SimleyShield styles

--- src/components/SmileyShield/index.js
+++ src/components/SmileyShield/index.js
@@ -6,7 +6,18 @@ class SmileyShield extends Component {
     const {teamShieldUrl, teamName}=this.props.componentData
     return (
       <div className="SmileyShield">
-        <img src={teamShieldUrl} alt={teamName} title={teamName} />
+        <img
+          className="personImage"
+          src={teamShieldUrl}
+          alt={teamName}
+          title={teamName}
+        />
+        <img
+          className="emojiImage"
+          src="/img/emojis/happy-2.png"
+          alt="Happy 2"
+          title="Happy 2"
+        />
       </div>
     )
   }
